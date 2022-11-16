1d53b7bc913d14a890230368039b2de71ad3984f
Oskar <oskar-fuernhammer@hotmail.com>
2022-11-16 08:51:30 +0100

    reactivated feedforward in PIDs

--- libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.cpp
+++ libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.cpp
@@ -344,14 +344,13 @@ void AC_AttitudeControl_Multi::rate_controller_run()
     Vector3f gyro_latest = _ahrs.get_gyro_latest();
 
     _motors.set_roll(get_rate_roll_pid().update_all(_ang_vel_body.x, gyro_latest.x, _motors.limit.roll) + _actuator_sysid.x);
-    //_motors.set_roll_ff(get_rate_roll_pid().get_ff());
-    _motors.set_roll_ff(-_rat_gyro_P*gyro_latest.y);
-
     _motors.set_pitch(get_rate_pitch_pid().update_all(_ang_vel_body.y, gyro_latest.y, _motors.limit.pitch) + _actuator_sysid.y);
-    //_motors.set_pitch_ff(get_rate_pitch_pid().get_ff());
-    _motors.set_pitch_ff(_rat_gyro_P*gyro_latest.x);
-
     _motors.set_yaw(get_rate_yaw_pid().update_all(_ang_vel_body.z, gyro_latest.z, _motors.limit.yaw) + _actuator_sysid.z);
+
+    // add cross coupling here (try to limit gyroscopic precession) rat_gyro_P is user settable and defaults to zero
+    _motors.set_roll_ff(get_rate_roll_pid().get_ff()   - _rat_gyro_P * gyro_latest.y);
+    _motors.set_pitch_ff(get_rate_pitch_pid().get_ff() + _rat_gyro_P * gyro_latest.x);
+
     _motors.set_yaw_ff(get_rate_yaw_pid().get_ff()*_feedforward_scalar);
 
     _sysid_ang_vel_body.zero();
