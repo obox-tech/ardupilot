06e9a533c70896c903c9e24dc63b8bdf2f4122f4
Oskar <oskar-fuernhammer@hotmail.com>
2022-12-04 21:14:30 +0100

    gyro comp

--- libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.cpp
+++ libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.cpp
@@ -349,11 +349,23 @@ void AC_AttitudeControl_Multi::rate_controller_run()
 
     Vector3f gyro_latest = _ahrs.get_gyro_latest();
 
-    _motors.set_roll(get_rate_roll_pid().update_all(_ang_vel_body.x, gyro_latest.x, _motors.limit.roll) + _actuator_sysid.x);
-    _motors.set_roll_ff(get_rate_roll_pid().get_ff());
+    // compute gyro feedforward torque_gyro = cross(I_rotor*rotor_angular_vel,gyr_latest)
+    float throttle_out = _motors.get_throttle_out();
+    // convert throttle_out to rotor angular velocity and multiply by rotor MoI
+    float rotor_angular_momentum;
+    rotor_angular_momentum =  0.058773f * (throttle_out - 0.1586f * throttle_out * throttle_out);
+    if (rotor_angular_momentum < 0){
+        rotor_angular_momentum = 0.0f;
+    }
 
-    _motors.set_pitch(get_rate_pitch_pid().update_all(_ang_vel_body.y, gyro_latest.y, _motors.limit.pitch) + _actuator_sysid.y);
-    _motors.set_pitch_ff(get_rate_pitch_pid().get_ff());
+
+    _motors.set_roll(get_rate_roll_pid().update_all(_ang_vel_body.x, gyro_latest.x, _motors.limit.roll)
+        + _actuator_sysid.x);
+    _motors.set_roll_ff(-_rat_gyro_P * rotor_angular_momentum * gyro_latest.y);
+
+    _motors.set_pitch(get_rate_pitch_pid().update_all(_ang_vel_body.y, gyro_latest.y, _motors.limit.pitch) 
+        + _actuator_sysid.y);
+    _motors.set_pitch_ff(_rat_gyro_P * rotor_angular_momentum * gyro_latest.x);
 
     _motors.set_yaw(get_rate_yaw_pid().update_all(_ang_vel_body.z, gyro_latest.z, _motors.limit.yaw) + _actuator_sysid.z);
     _motors.set_yaw_ff(get_rate_yaw_pid().get_ff()*_feedforward_scalar);
